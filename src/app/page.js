3d4b9af5c60464c13fcb02abfb4cf34d4de30366
abbasinfoicon <uid3@infoiconsoftware.com>
2024-02-23 12:28:32 +0530

    link update

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
             <div className="row align-items-center">
               <div className="col-lg-8 col-sm-12">
                 <h3 className="text-white">“You cannot teach
-                  <span className="ps-5 ps-10">a person anything,</span>
+                  <span className="ps-5">a person anything,</span>
                   <span className="ps-4">you can only help them </span>
                   <span className="ps-5">find it within themselves”</span>
                 </h3>
@@ -26,21 +26,21 @@ export default function Home() {
         <div className="container">
           <div className="row">
             <div className="col-lg-4">
-              <Link href="holischtische-massage.php" className="blok">
+              <Link href="holischtische-massage" className="blok">
                 <img src="./assets/img/beeldmerk.png" alt="" />
                 <h2>Behandeling 1</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur mollitia el.</p>
               </Link>
             </div>
             <div className="col-lg-4">
-              <Link href="retreat.php" className="blok" style={{ background: '#ffffff4d' }}>
+              <Link href="retreat" className="blok" style={{ background: '#ffffff4d' }}>
                 <img src="./assets/img/beeldmerk.png" alt="" />
                 <h2>Behandeling 1</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur mollitia el.</p>
               </Link>
             </div>
             <div className="col-lg-4">
-              <Link href="reviews.php" className="blok">
+              <Link href="reviews" className="blok">
                 <img src="./assets/img/beeldmerk.png" alt="" />
                 <h2>Behandeling 1</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur mollitia el.</p>
@@ -61,7 +61,7 @@ export default function Home() {
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis culpa sit eaque tenetur
                 debitis repudiandae numquam illum reiciendis aspernatur quasi consequatur similique deserunt nam
                 maiores fuga molestiae, nobis dolor adipisci unde laudantium? Explicabo, voluptates totam!</p>
-              <Link href="over-mij.php">Lees meer <img src="./assets/img/angles-right-solid.svg" alt="" style={{ width: '14px', filter: ' opacity(0.5)' }} /></Link>
+              <Link href="over-mij">Lees meer <img src="./assets/img/angles-right-solid.svg" alt="" style={{ width: '14px', filter: ' opacity(0.5)' }} /></Link>
             </div>
           </div>
         </div>
